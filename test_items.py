18a85f926997f50c33f14c35ad2907db1168c6f0
xsmit <55706442+xsmit@users.noreply.github.com>
2023-04-09 15:34:42 +0700

    Fix selector

--- test_items.py
+++ test_items.py
@@ -10,6 +10,6 @@ def test_check_if_btn_add_to_cart_is_present(browser):
     browser.get(link)
     time.sleep(30)
     try:
-        browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > ubutton")
+        browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")
     except NoSuchElementException:
         assert False, "Button 'Add to cart' does not exist"
